dbd579b500db210f6f5f793eefc04dccaf0c5454
ANANT-RAI1101 <anantrai1101@gmail.com>
2025-12-17 18:43:21 +0530

    created the basic structure of the project

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express=require('express')
+const bodyParser=require('body-parser')
+
+const{PORT}=require("./config/server-config")
+
+const setupAndStartServer=async(req,res)=>{
+    const app=express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+    app.listen(PORT,async()=>{
+        console.log(`server is running at ${PORT}`);
+        
+    })
+}
+
+setupAndStartServer();
